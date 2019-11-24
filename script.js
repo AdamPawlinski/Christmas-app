8b55e5ccd0067fb68c864c46cbb423f4b35c823a
Adam Pawliński <adam.pawlinski@gmail.com>
2019-11-24 11:01:09 +0100

    Changes in showing results made

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let userTabStart = ['Adam', 'Krzyś', 'Ania', 'Grześ', 'Karola'];
+    let drawResultsState = [];
+    let userTabStart = ['Adam', 'Krzysztof', 'Anna', 'Grzegorz', 'Karolina'];
+    let userEmail = ['adam.pawlinski@gmail.com', 'adam.pawlinski@gmail.com', 'adam.pawlinski@gmail.com', 'adam.pawlinski@gmail.com', 'adam.pawlinski@gmail.com'];
     let drawUserTab = userTabStart.slice();
     let drawResultArr = [];
 
@@ -7,13 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
     buttonStart.innerText = 'Start the draw';
     buttonStart.classList.add('btn', 'btn-primary', 'btn-lg', 'd-flex', 'justify-content-between');
     buttonStart.style.setProperty('role', 'button');
-    buttonStart.addEventListener('click', () => {
-        // let content = document.querySelector('.results');
-        // if (resultTable != 'undefined') {
-        //     content.removeChild('ul'); 
-        //     let showResults = '';
-        // }        
-        beginDraw();
+    buttonStart.addEventListener('click', () => {               
+        beginDraw();                    
     })
     let header = document.querySelector('.header');
     header.style.background = 'url("gifts.jpg")';
@@ -21,65 +18,118 @@ document.addEventListener('DOMContentLoaded', () => {
     header.appendChild(buttonStart);
     
     let beginDraw = () => {     
-        let drawResult; 
+        let drawResult;   
+        let resultTableExisting = document.getElementById('results-table');
+        console.log(resultTableExisting);                                  
+        resultTableExisting ===  null ? null : resultTableExisting.parentNode.removeChild(resultTableExisting);      
         if (userTabStart.length < 2)
         {
             console.log('Too less users to make the draw');
         } else {       
             for (let i of userTabStart){         
                 if (drawUserTab.length === 1 && drawUserTab[0] === i) {
-                    drawResultArr = [];
+                    drawResultArr = [];   
                     drawUserTab = userTabStart.slice();
                     beginDraw();
                     break;
                 }           
-                do { 
+                do {                                         
                     drawResult = Math.floor(Math.random() * drawUserTab.length); 
                 } while (drawUserTab[drawResult] === i);                  
                 const drawUser = drawUserTab.splice(drawResult, 1);                
-                drawResultArr.push(`${i}:${drawUser}`); 
+                drawResultArr.push(`${i}:${drawUser}`);               
             }
         }
+        localStorage.setItem('draw', JSON.stringify(drawResultArr))
+        drawResultsState = JSON.parse(localStorage.getItem('draw'))
         return showResults(drawResultArr);
     }
 
     
 
-    showResults = (drawResultArr) => {
-        let content = document.querySelector('.results');
-        let resultTable = document.createElement('ul');
-        resultTable.classList.add('list-group', 'result-table', 'tab-content');
+    showResults = (drawResultArr) => {        
+        let person = prompt('Wybierz osobę', 'Adam');
+        switch(person) {
+            case "Adam":
+              result = drawResultArr.find(
+                  res => res.split(':')[0] === "Adam"
+              );
+              break;
+            case "Karolina":
+            result = drawResultArr.find(
+                res => res.split(':')[0] === "Karolina"
+            );
+              break;
+            case "Grzegorz":
+            result = drawResultArr.find(
+                res => res.split(':')[0] === "Grzegorz"
+            );
+              break;
+              case "Krzysztof":
+              result = drawResultArr.find(
+                res => res.split(':')[0] === "Krzysztof"
+            );
+              break;
+              case "Anna":
+              result = drawResultArr.find(
+                res => res.split(':')[0] === "Anna"
+            );
+              break;
+            default:
+            result = "Błędny wybór";
+          }
+          let resultShow = result.split(':')[1]
+          alert(`Wylosowałeś/aś: ${resultShow}`)
 
-        for (let j of drawResultArr) {
-            let userResult = j;
-            let userResultSplit = userResult.toString().split(':');
-            let resultItem = document.createElement('li');
-            resultItem.classList.add('list-group-item', 'd-flex', 'flex-fill','justify-content-between', 'order-1');
-            resultItem.innerHTML = `<span class='user'> ${userResultSplit[0]} </span>`;
+          showResults()
+        // let content = document.querySelector('.results');
+        // let resultTable = document.createElement('ul');
+        // resultTable.classList.add('list-group', 'result-table', 'tab-content');
+        // resultTable.setAttribute('id', 'results-table');
+        // for (let userResult of drawResultArr) {
+        //     let userResultSplit = userResult.toString().split(':');
+        //     let resultItem = document.createElement('li');
+        //     resultItem.classList.add('list-group-item', 'd-flex', 'flex-fill','justify-content-between', 'order-1');
+        //     resultItem.innerHTML = `<span class='user'> ${userResultSplit[0]} </span>`;
             
-            let button = document.createElement('button'); 
-            button.innerText = 'Show / hide';
-            button.classList.add('btn', 'btn-success','btn-sm', 'd-flex', 'order-3');
-            button.dataset.dataToggle = button;
-            resultItem.appendChild(button);
-            resultTable.appendChild(resultItem);
+        //     let button = document.createElement('button'); 
+        //     button.innerText = 'Show / hide';
+        //     button.classList.add('btn', 'btn-success','btn-sm', 'd-flex', 'order-3');
+        //     button.dataset.dataToggle = button;
 
-            let resultItemUserDraw = document.createElement('span');
-            resultItemUserDraw.classList.add('user-draw', 'd-flex', 'order-2');
-            resultItemUserDraw.style.visibility = 'hidden';
-            resultItemUserDraw.innerHTML =  `${userResultSplit[1]}`;
-            button.addEventListener('click', () => {        
-                if (resultItemUserDraw.style.visibility === 'hidden') {
-                    resultItem.appendChild(resultItemUserDraw);
-                    resultItemUserDraw.style.visibility = 'visible';
-                } else {
-                    resultItemUserDraw.style.visibility = 'hidden';
-                }
-            });
-             
-                            
-        }
+            
+            // let buttonSend = document.createElement('button'); 
+            // button.innerText = 'Send email';
+            // button.classList.add('btn', 'btn-success','btn-lg', 'd-flex', 'order-4');
+            // button.dataset.dataToggle = button;
+            // let email = userEmail[j];
+            // buttonSend.addEventListener('click', () => {        
+            //     let link = `mailto:${email}&subject=Christmas app draw result&body=You have just draw ${userResultSplit[1]} to prepare the gift for`;
+            //     window.location.href = link;
+            // });
+            
+            // resultItem.appendChild(buttonSend);
 
-        content.appendChild(resultTable);
+
+
+
+
+        //     resultItem.appendChild(button);
+        //     resultTable.appendChild(resultItem);
+
+        //     let resultItemUserDraw = document.createElement('span');
+        //     resultItemUserDraw.classList.add('user-draw', 'd-flex', 'order-2');
+        //     resultItemUserDraw.style.visibility = 'hidden';
+        //     resultItemUserDraw.innerHTML =  `${userResultSplit[1]}`;
+        //     button.addEventListener('click', () => {        
+        //         if (resultItemUserDraw.style.visibility === 'hidden') {
+        //             resultItem.appendChild(resultItemUserDraw);
+        //             resultItemUserDraw.style.visibility = 'visible';
+        //         } else {
+        //             resultItemUserDraw.style.visibility = 'hidden';
+        //         }
+        //     });         
+        // }
+        // content.appendChild(resultTable);
     }
 });
